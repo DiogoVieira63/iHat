1f05c79876321019fff8f0ec169b5b1a9f35a86c
joaotorres01 <joao-torres01@hotmail.com>
2023-10-26 18:17:12 +0100

    add Registar Capacete fronted

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import FormObra from "../components/FormObra.vue"
+import FormCapacete from "../components/FormCapacete.vue"
 </script>
 
 <template>
   <FormObra />
+  <FormCapacete />
 </template>
 
