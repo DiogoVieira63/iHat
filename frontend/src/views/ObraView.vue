f4798475fdef3a9d4c3319c3274f398575bf90bc
diogovieira63 <a93251@alunos.uminho.pt>
2023-10-28 16:50:27 +0100

    Start Obra View

---
+++ frontend/src/views/ObraView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Lista from "../components/Lista.vue"
+import PageLayout from "../components/PageLayout.vue"
+import { computed, ref, watch } from "vue"
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+</script>
+
+<template>
+  <PageLayout>
+    <v-row>
+        <v-col cols="6">
+            <h1>{{ route.params.id }}</h1>
+        </v-col>
+        <v-col cols="6">
+            <h1>{{ route.params.id }}</h1>
+        </v-col>
+    </v-row>
+  </PageLayout>
+</template>
+
