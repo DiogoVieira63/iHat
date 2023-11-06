56d43262c4dcbf742c698a59a4e43764e2bbae4b
diogovieira63 <a93251@alunos.uminho.pt>
2023-11-06 11:59:13 +0000

    add sort and filter functionality | change rowObra switch to select

--- frontend/src/components/RowObra.vue
+++ frontend/src/components/RowObra.vue
@@ -10,41 +10,37 @@ const props = defineProps({
         required: true
     },
 })
+const emit = defineEmits(['removeCapacete'])
 
 const router = useRouter()
 const dialog = ref(false)
-const estado = ref(currentEstado(props.row.Estado))
 const removeDialog = ref(false)
 
 function changePage(id) {
     router.push({ path: `/capacete/${id}` })
 }
 
-function newEstado(value) {
-    return value === "Livre" ? "Não Operacional" : "Livre"
-
-}
-
-function currentEstado(value) {
-    return value !== "Não Operacional" ? true : false
-}
-
 function changeEstado(confirmation) {
     if (confirmation) {
         props.row.Estado = newEstado(props.row.Estado)
     }
-    else {
-        estado.value = !estado.value
-    }
     dialog.value = false
 }
 
-const emit = defineEmits(['removeCapacete'])
+function newEstado(estado) {
+    return estado === "Livre" ? "Não Operacional" : "Livre"
+}
+
 
 function removeCapacete(confirmation) {
     removeDialog.value = false
     if (confirmation)
         emit('removeCapacete', props.row.id)
+}
+
+function possibleChangeEstado(value, open) {
+    if (value !== props.row['Estado'])
+        open()
 }
 
 function isInUso() {
@@ -54,32 +50,31 @@ function isInUso() {
 </script>
 <template>
     <td @click="changePage(props.row.id)">{{ props.row['id'] }}</td>
-    <td>{{ props.row['Estado'] }}</td>
     <td>
-        <v-row>
-            <v-col cols="5">
-                <confirmation title="Confirmação" :function="changeEstado">
-                    <template #button="{ prop }">
-                        <v-switch v-bind="prop" v-model="estado" :disabled="row.Estado == 'Em uso'" color="info"></v-switch>
-                    </template>
-                    <template v-slot:message>
-                        Tem a certeza que pretende mudar o Estado do<strong> Capacete {{ props.row.id }}</strong> de
-                        <span class='text-red'> {{ props.row.Estado }}</span> para
-                        <span class="text-green"> {{ newEstado(props.row['Estado']) }}?</span>
-                    </template>
-                </confirmation>
-            </v-col>
-            <confirmation title="Confirmação"  :function="removeCapacete">
-                <template #button="{ prop }">
-                    <v-btn v-bind="prop" color="grey" text class="mt-6" :disabled="isInUso()">
-                        <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                </template>
-                <template v-slot:message>
-                    Tem a certeza que pretende remover o <strong> Capacete {{ props.row.id }}</strong> da obra?
-                </template>
-            </confirmation>
-        </v-row>
+        <confirmation title="Confirmação" :function="changeEstado">
+            <template #button="{ open }">
+                <v-select :disabled="isInUso()" class="mt-5 pa-0" density="compact" :items="['Livre', 'Não Operacional']"
+                    :model-value="props.row['Estado']" @update:model-value="(value) => possibleChangeEstado(value, open)">
+                </v-select>
+            </template>
+            <template v-slot:message>
+                Tem a certeza que pretende alterar o estado do <strong> Capacete {{ props.row.id }}</strong> de
+                <span class="font-weight-bold text-red">{{ props.row['Estado'] }}</span> para
+                <span class="font-weight-bold text-green">{{ newEstado(props.row['Estado']) }}</span>?
+            </template>
+        </confirmation>
+    </td>
+    <td>
+        <confirmation title="Confirmação" :function="removeCapacete">
+            <template #button="{ prop }">
+                <v-btn v-bind="prop" color="grey" text :disabled="isInUso()">
+                    <v-icon>mdi-delete</v-icon>
+                </v-btn>
+            </template>
+            <template v-slot:message>
+                Tem a certeza que pretende remover o <strong> Capacete {{ props.row.id }}</strong> da obra?
+            </template>
+        </confirmation>
     </td>
 </template>
 
